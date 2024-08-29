f849241e40493cc43ae7d1a1f376b9b83ddb3343
Sounak Ghosh <145130712+Sounakghosh45@users.noreply.github.com>
2024-08-29 23:21:12 +0530

    Initial commit

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a,g;
+   printf("Enter two numbers:");
+   scanf("%d %d",&a,&g);
+   int c=a+g;
+   printf("%d",c);
+}//
+// Created by User on 29-08-2024.
+//
